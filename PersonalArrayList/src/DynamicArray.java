3eaad78e972f8af5a87127411043b8377fcf8dc4
Andrew Ralph <ralpaa@farmingdale.edu>
2026-03-11 11:16:30 -0400

    some of the test methods for main are complete

--- PersonalArrayList/src/DynamicArray.java
+++ PersonalArrayList/src/DynamicArray.java
@@ -1,198 +1,185 @@
 public class DynamicArray<E> {
-    private E[] array;
-    private int size;
-    private int capacity;
-
+    protected E[] baseArray;
+    protected int size;
+    protected int capacity;
 
     public DynamicArray() { //default constructor
         capacity = 10;
-        array = (E[]) new Object[capacity];
+        baseArray = (E[]) new Object[capacity];
         size = 0;
     }
-
     public DynamicArray(int capacity) { //when capacity is declared this runs
         this.capacity = capacity;
-        array = (E[]) new Object[capacity];
+        baseArray = (E[]) new Object[capacity];
         size = 0;
     }
-
     public boolean isEmpty() {
         if (size == 0) {
             return true;
-        } //if size is 0 array is empty
+        } //if size is 0 baseArray is empty
         return false;
 
     }
-
-    public int size() { // just returns the size of the array not starting from 0
+    public int size() { // just returns the size of the baseArray not starting from 0
         return size;
     }
-
     public void doubleCapacity() {
         capacity *= 2;
         E[] newArray = (E[]) new Object[capacity];
-        for (int i = 0; i < size; i++) { // cycles through the array and stores each value into the new array
-            newArray[i] = array[i];
+        for (int i = 0; i < size; i++) { // cycles through the baseArray and stores each value into the new baseArray
+            newArray[i] = baseArray[i];
         }
-        array = newArray; // set array to the new array that has double capacity
+        baseArray = newArray; // set baseArray to the new baseArray that has double capacity
     }
-
     public void halveCapacity() {
-        if (capacity / 2 >= size) { // the capacity should still be bigger than array size
+        if (capacity / 2 >= size) { // the capacity should still be bigger than baseArray size
             capacity /= 2;
             E[] newArray = (E[]) new Object[capacity];
-            for (int i = 0; i < size; i++) { // cycles through the array and stores each value into the new array
-                newArray[i] = array[i];
+            for (int i = 0; i < size; i++) { //cycles through the baseArray and stores
+                newArray[i] = baseArray[i];  //each value into the new baseArray
             }
-            array = newArray;
+            baseArray = newArray;
         }
     }
-
     public void addLast(E e) {
-        if (size == capacity) { // when adding if the array is to small we double its capacity
+        if (size == capacity) { // when adding if the baseArray is to small we double its capacity
             doubleCapacity();
         }
-        array[size] = e; // input the new variable at the end
-        size++;          // expand array
+        baseArray[size] = e; // input the new variable at the end
+        size++;          // expand baseArray
     }
-
     public void addFirst(E e) {
-        if (size == capacity) { // when adding if the array is to small we double its capacity
+        if (size == capacity) { // when adding if the baseArray is to small we double its capacity
             doubleCapacity();
         }
-        for (int i = size; i > 0; i--) { //starting from the tail of the array it goes to the head
-            array[i] = array[i - 1];     //and shifts each value downwards
+        for (int i = size; i > 0; i--) { //starting from the tail of the baseArray it goes to the head
+            baseArray[i] = baseArray[i - 1];     //and shifts each value downwards
         }
-        array[0] = e; // input the new variable at the head
-        size++;       // expand array
+        baseArray[0] = e; // input the new variable at the head
+        size++;       // expand baseArray
     }
-
     public void insertAt(int i, E e) {
-        if (size == capacity) { // when adding if the array is to small we double its capacity
+        if (size == capacity) { // when adding if the baseArray is to small we double its capacity
             doubleCapacity();
         }
-        if (i < 0 || i > size) { // checks to see if the index given exists within the array
+        if (i < 0 || i > size) { // checks to see if the index given exists within the baseArray
             System.out.println("index is out of bounds");
         } else {
             for (int j = size; j > i; j--) { // starting from the tail we go towards the index
-                array[j] = array[j - 1];     // given and shift the values downwards
+                baseArray[j] = baseArray[j - 1]; // given and shift the values downwards
             }
-            array[i] = e; // input the new variable at the index given
-            size++;       // expand array
+            baseArray[i] = e; // input the new variable at the index given
+            size++;       // expand baseArray
         }
     }
-
     public E removeFirst() {
-        if (size == 0) { // if the array is empty we return null as nothing can be removed
+        if (size == 0) { // if the baseArray is empty we return null as nothing can be removed
             return null;
         }
-        E firstNum = array[0]; // store the first number to return it
-        for (int i = 0; i < size - 1; i++) { // shift the array
-            array[i] = array[i + 1];         // the plus 1 ignores the first index when restoring numbers
+        E firstNum = baseArray[0]; // store the first number to return it
+        for (int i = 0; i < size - 1; i++) { // shift the baseArray
+            baseArray[i] = baseArray[i + 1]; // the plus 1 ignores the first index when restoring numbers
         }
-        size--; //contract array
+        size--; //contract baseArray
         return firstNum;
-
     }
-
     public E removeLast() {
-        if (size == 0) { // if the array is empty we return null as nothing can be removed
+        if (size == 0) { // if the baseArray is empty we return null as nothing can be removed
             return null;
         }
-        E lastNum = array[size - 1]; // the array is capped at size - 1 not size
+        E lastNum = baseArray[size - 1]; // the baseArray is capped at size - 1 not size
         size--;
         return lastNum;
     }
-
     public E removeAt(int i) {
-        if (size == 0) {return null;} // if the array is empty we return null as nothing can be removed
-        if (i < 0 || i >= size) { // checks to see if the index given exists within the array
+        if (size == 0) {return null;} // if the baseArray is empty we return null as nothing can be removed
+        if (i < 0 || i >= size) { // checks to see if the index given exists within the baseArray
             System.out.println("index is out of bounds");
         } else {
-            E removeNum = array[i];
+            E removeNum = baseArray[i];
             for (int j = i; j < size - 1; j++) { // left shift
-                array[j] = array[j + 1];
+                baseArray[j] = baseArray[j + 1];
 
 
             }
-            size--; // contract array
+            size--; // contract baseArray
             return removeNum;
         }
-        return null;
+        return null; //placeholder return outside of loop so that the code produces no error
     }
     public E getFirst(){
-        if(size == 0){return null;}// if the array is empty we return null
-        return array[0];
+        if(size == 0){return null;}// if the baseArray is empty we return null
+        return baseArray[0];
     }
     public E get(int i){
-        if(size == 0){return null;}
-        if (i < 0 || i >= size) {
+        if(size == 0){return null;} // if the baseArray is empty we return null
+        if (i < 0 || i >= size) { // checks to see if the index given exists within the baseArray
            System.out.println("index is out of bounds");
            return null;
         }
-        return array[i];
+        return baseArray[i];
     }
     public E set(int i, E newElem){
-        if(size == 0){return null;}
-        if (i < 0 || i >= size) {
+        if(size == 0){return null;} // if the baseArray is empty we return null
+        if (i < 0 || i >= size) {   // checks to see if the index given exists within the baseArray
             System.out.println("index is out of bounds");
             return null;
         }
-        E oldElem = array[i];
-        array[i] = newElem;
-        return oldElem;
+        E oldElem = baseArray[i];
+        baseArray[i] = newElem; //assigns new element to old element index
+        return oldElem; //prints out element that was their previously
     }
     public E[] toArray(){
         E[] newArray = (E[]) new Object[size];
-        for (int i = 0; i < size; i++) {
-            newArray[i] = array[i];
+        for (int i = 0; i < size; i++) { // newArray takes the baseArrays elements
+            newArray[i] = baseArray[i];      // but does not change baseArray
         }
         return newArray;
     }
-    public void trimToSize(){
+    public void trimToSize(){ // capacity = size
         capacity = size;
         E[] newArray = (E[]) new Object[capacity];
-        for(int i = 0; i < size; i++){
-            newArray[i] = array[i];
+        for(int i = 0; i < size; i++){ //copy's baseArray to newArray to update capacity
+            newArray[i] = baseArray[i];
         }
-        array = newArray;
+        baseArray = newArray;
     }
-    public DynamicArray<E> clone(){
+    public DynamicArray<E> clone(){ //makes a new dynamicArray with same values as the base one
         DynamicArray<E> newList = new DynamicArray<E>(capacity);
-        for(int i = 0; i < size; i++){
-            newList.array[i] = array[i];
+        for(int i = 0; i < size; i++){ //copy's baseArray to the newList's baseArray
+            newList.baseArray[i] = baseArray[i];
         }
-        newList.capacity = capacity;
-        newList.size = size;
-        return newList;
+        newList.capacity = capacity; // copy capacity
+        newList.size = size;         // copy size
+        return newList;              //return list
     }
     public void clear(){
-        for(int i = 0; i < size; i++){
-            array[i] = null;
+        for(int i = 0; i < size; i++){ // clears baseArray contents
+            baseArray[i] = null;
         }
         size = 0;
     }
     public String toString(){
-        StringBuilder sb = new StringBuilder("[");
-        for(int i = 0; i < size; i++){
-            sb.append(array[i]);
+        StringBuilder sb = new StringBuilder("["); // start string with [
+        for(int i = 0; i < size; i++){ // this will add each value in the array into
+            sb.append(baseArray[i]);   // the string with a comma between each
             if(i < size-1){
                 sb.append(",");
             }
         }
-        sb.append("]");
-        return sb.toString();
+        sb.append("]"); // end string with ]
+        return sb.toString(); //returns complete String
 
     }
-    public boolean equals(Object o){
-        if(this == o){return true;}
-        if(o == null || !(o instanceof DynamicArray)){return false;}
-        DynamicArray<E> other = (DynamicArray<E>) o;
-        if(size != other.size){return false;}
-        for(int i = 0; i < size; i++){
-            if(!array[i].equals(other.array[i])){return false;}
+    public boolean equals(Object o){ // compares this DynamicArray to an object for equality
+        if(this == o){return true;} // if both references point to the same address they are equal
+        if(o == null || !(o instanceof DynamicArray)){return false;} // if 0 is null or not a DynamicArray, they're not equal
+        DynamicArray<E> other = (DynamicArray<E>) o; // cast o to dynamicArray for access to its contents
+        if(size != other.size){return false;} // compares size, if not the same not equal
+        for(int i = 0; i < size; i++){ // compares every element and determines if they are equal
+            if(!baseArray[i].equals(other.baseArray[i])){return false;}
         }
-        return true;
+        return true; // all elements equal
 
     }
 }
--- PersonalArrayList/src/Main.java
+++ PersonalArrayList/src/Main.java
@@ -1,4 +1,64 @@
 public class Main {
+    public static void testIsEmpty(){
+        DynamicArray<Integer> list = new DynamicArray<>();
+        System.out.println("--- Testing isEmpty ---");
+        System.out.println("Is list empty? " + list.isEmpty()); // should print true
+        list.addLast(10);
+        System.out.println("Added 10, is list empty? " + list.isEmpty()); // should print false
+    }
+    public static void testSize(){
+        DynamicArray<Integer> list = new DynamicArray<>();
+        System.out.println("--- Testing size ---");
+        System.out.println("What is the list size? " + list.size()); // 0
+        list.addLast(10);
+        System.out.println("What is the list size " + list.size()); // 1
+    }
+    public static void testDoubleCapacity(){
+        DynamicArray<Integer> list = new DynamicArray<>();
+        System.out.println("--- Testing doubleCapacity ---");
+        System.out.println("What is the list capacity? " + list.capacity); // will be 10
+        list.doubleCapacity();
+        System.out.println("What is the list capacity " + list.capacity); // will be 20
+    }
+    public static void testHalveCapacity(){
+        DynamicArray<Integer> list = new DynamicArray<>();
+        System.out.println("--- Testing halveCapacity ---");
+        System.out.println("What is the list capacity? " + list.capacity); // will be 10
+        list.halveCapacity();
+        System.out.println("What is the list capacity " + list.capacity); // will be 5
+    }
+    public static void testAddLast(){
+        DynamicArray<Integer> list = new DynamicArray<>(); //populate the list
+        list.addFirst(10); list.addFirst(20); list.addFirst(30); list.addFirst(40); list.addFirst(50);
+        int size = list.size;
+        System.out.println("--- Testing addLast ---");
+        System.out.println("What is the last integer in the array? " + list.baseArray[size]); // will be 50
+        list.addLast(60);
+        size = list.size;
+        System.out.println("What is the last integer in the array? " + list.baseArray[size]); // will be 60
+    }
+    public static void testAddFirst(){
+        DynamicArray<Integer> list = new DynamicArray<>(); //populate the list
+        list.addLast(10); list.addLast(20); list.addLast(30); list.addLast(40); list.addLast(50);
+        System.out.println("--- Testing addFirst ---");
+        System.out.println("What is the first integer in the array? " + list.baseArray[0]); // will be 10
+        list.addFirst(60);
+        System.out.println("What is the first integer in the array? " + list.baseArray[0]); // will be 60
+    }
+    public static void testInsertAt(){
+        DynamicArray<Integer> list = new DynamicArray<>(); //populate the list
+        list.addLast(10); list.addLast(20); list.addLast(30); list.addLast(40); list.addLast(50);
+        System.out.println("--- Testing insertAt ---");
+        System.out.println("What is the integer at index 2 in the array? " + list.baseArray[2]); // will be 30
+        list.insertAt(2,60);
+        System.out.println("What is the integer at index 2 in the array? " + list.baseArray[2]); // will be 60
+    }
+    public static void testRemoveFirst(){
+
+    }
+
+
+
     public static void main(String[] args){
 
     }
